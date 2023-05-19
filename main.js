9a2cad968255f1708e165bd94f207b9291143c0a
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-05-19 17:42:53 +0100

    added delete button and additional functionalities

--- main.js
+++ main.js
@@ -2,30 +2,60 @@ let myLeads = []
 const inputElement = document.querySelector("#input-element")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.querySelector("#del-btn")
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
 
 inputBtn.addEventListener("click", function () {
     if (inputElement.value !== '') {
         myLeads.push(inputElement.value)
         inputElement.value = ''
-        renderLeads()
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
     }
 
 })
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        // const li = document.createElement("li")
-        // li.textContent = myLeads[i]
-        // ulEl.append(li)
-        listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-            </a>
-        </li>
-        `
-    }
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-    ulEl.innerHTML = listItems
-}
+
+
+
+
+
+// // myLeads = JSON.parse(myLeads)
+// // myLeads.push("Herald")
+// // myLeads = JSON.stringify(myLeads)
+// // console.log(typeof myLeads)
+
+// // localStorage.setItem("name", "Per Borgen")
+// // console.log(localStorage.getItem("name"))
+// // localStorage.clear()
+
+
+
+// // const li = document.createElement("li")
+//         // li.textContent = myLeads[i]
+//         // ulEl.append(li)
