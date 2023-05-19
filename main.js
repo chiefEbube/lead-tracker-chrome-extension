70c59e2d60e9eb17b4c0db225bb55f9f33c8aa4b
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-05-19 01:17:59 +0100

    added functionalities

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
+let myLeads = []
+const inputElement = document.querySelector("#input-element")
 const inputBtn = document.querySelector("#input-btn")
-inputBtn.addEventListener("click", function(){
-    console.log("Button clicked")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function () {
+    if (inputElement.value !== '') {
+        myLeads.push(inputElement.value)
+        inputElement.value = ''
+        renderLeads()
+    }
+
 })
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
+    }
+
+    ulEl.innerHTML = listItems
+}
